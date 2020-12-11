1c619193fd041ba877d24f80ae24b9010d684313
Sayna Parsi <parsis@uw.edu>
2020-12-10 21:54:06 -0800

    adding readme details and cleaning up logs

--- README.md
+++ README.md
@@ -1,3 +1,41 @@
 # Bike and Ride
-chrome extension to show commute time using bike and public transit for home buyers on Redfin.
+<img src="https://github.com/sainap/bike-and-ride/blob/main/lib/icon128.png" align="left" alt="Bike and Ride logo by Sayna Parsi">
+Chrome extension to show commute time using a bike and public transit for home buyers on Redfin.
 
+This project was inspired by the process of home shopping. Finding a home is tough, but finding a home that has a reasonably short commute - if you don't drive is a whole different story.
+
+![Bike and Ride screenshot example](https://chrome.google.com/webstore/detail/bikeride/infhpbjmcjhdbchempmppkolaaggebko?hl=en&authuser=0)
+
+
+
+### Built With
+This project was built using:
+* [HERE Technologies Geocoding API](https://developer.here.com/documentation/geocoding-search-api/dev_guide/topics/quick-start.html): to get the latitude and longitude for the home address and commute locations.
+* [HERE Technologies Intermodal Routing API](https://developer.here.com/documentation/intermodal-routing/dev_guide/private-bike-and-ride/index.html): to calculate commute duration,
+* [AWS Lambda](https://aws.amazon.com/lambda/): to mask the API key for API calls and throttle usage.
+
+
+### How does it work?
+First, in `main.js` we identify the address of the home and commute locations based on the JS path of the elements. We then geocode these addresses to pass latitude and longitudes of them to the intermodal routing API to ultimately get the duration. We send a `message` to `background.js` where the actual call to the lambda is made.
+
+Here's an example of data from a geocoding call:
+```
+{ lat: 47.66507, lng: -122.3025 }
+```
+Here's an example of data from an intermodal routing call:
+```
+{duration: "20 mins"}
+```
+ultimately, `main.js` creates a new element on the page to display the bike and ride duration.
+
+Note: The API call to intermodal routing currently calculates the duration of the route for a scenario where arrival is set to 8 am for the day that the call is made. I am not sure how Redfin estimates the commute time, but I think they calculate it during the commute hours based on a few examples I checked manually. You may occasionally get commutes that are longer than the commute listed by Redfin the reason is that we are calculating the commute at different times so this is partially why! :)
+
+
+### License
+
+Distributed under the GNU v3 license. See `LICENSE` for more information.
+
+
+### Contact
+
+Sayna Parsi - [@sainaparsi](https://twitter.com/sainaparsi)
--- main.js
+++ main.js
@@ -39,7 +39,6 @@ function getDestinations() {
 // returns geocoordinates for a given address
 // for example {lat:123.4, lng: 123,4}
 function geocode(location) {
-  console.log(3);
   return new Promise((resolve, reject) => {
     // eslint-disable-next-line no-undef
     chrome.runtime.sendMessage(
@@ -57,7 +56,6 @@ function geocode(location) {
 // takes lat long objects for origin and destination
 // returns commute duration in seconds
 async function getCommuteDuration(origin, destination) {
-  console.log(2);
   return new Promise((resolve, reject) => {
     // eslint-disable-next-line no-undef
     chrome.runtime.sendMessage(
@@ -74,7 +72,6 @@ async function getCommuteDuration(origin, destination) {
 
 // displays bikeride commute on page
 function displayBikeRide(duration, address) {
-  console.log(1);
   const commuteCollection = document.getElementsByClassName('Commutes font-size-base');
   const commuteParent = commuteCollection[0];
   let element = `<div class="Commute basic-border-radius"><span class="field select Select icon withFlyout withOptions mounted selected clickable optional" data-rf-test-name="Select"><span data-rf-test-name="Select" class="input" role="listbox" tabindex="0"><span class="container" style="margin: 0;padding: 0;display: flex;"><svg class="SvgIcon bikescore"><svg viewBox="0 0 24 24"><path d="M22.122 11.284c-1.817-1.505-3.316-1.451-4.647-1.022L15.17 6h3.58a.25.25 0 0 0 .25-.25v-1.5a.25.25 0 0 0-.25-.25h-6.835a.25.25 0 0 0-.221.367L13.615 8H8l-.641-1.016L8 7V5.25A.25.25 0 0 0 7.75 5h-4.5a.25.25 0 0 0-.25.25v1.5c0 .138.112.25.25.25h1.828L6.39 9.056l-.566 1.027A4.981 4.981 0 0 0 5 10a5 5 0 0 0-5 5c0 2.703 2.297 5 5 5a5 5 0 0 0 5-5c0-.677-.138-1.321-.382-1.91l5.261-3.045.744 1.289a4.97 4.97 0 0 0-1.512 4.727c.399 1.919 1.962 3.468 3.887 3.842A5.006 5.006 0 0 0 24 15.046c.013-1.474-.742-2.822-1.878-3.762zm-14.929 5.91C4.73 19.087 2 17.356 2 15a2.993 2.993 0 0 1 2.754-2.975l-1.2 2.179a.244.244 0 0 0 .007.254c.189.288.667 1.002.899 1.348a.25.25 0 0 0 .332.077l3.069-1.776c.288.92.147 2.027-.668 3.087zm1.321-5.747a5.025 5.025 0 0 0-.855-.667l.392-.751h2.978l-2.515 1.418zm9.985 6.512a3.021 3.021 0 0 1-2.414-2.249 2.953 2.953 0 0 1 .581-2.571l1.343 2.327a.25.25 0 0 0 .342.092l1.305-.754a.249.249 0 0 0 .094-.335l-1.305-2.413c.858-.161 1.813-.057 2.99 1.439.318.405.548.898.564 1.413a3.005 3.005 0 0 1-3.5 3.051z" fill-rule="evenodd"></path></svg></svg>
@@ -91,8 +88,11 @@ async function main() {
       const homeaddress = getHomeAddress();
 
       geocode(homeaddress).then((homeCoord) => {
+        console.log(homeCoord);
         destinations.forEach((destination) => {
+          console.log(destination);
           geocode(destination).then((destinationCoord) => {
+            console.log(destinationCoord);
             getCommuteDuration(homeCoord, destinationCoord).then((duration) => {
               displayBikeRide(duration.duration, destination);
             });
